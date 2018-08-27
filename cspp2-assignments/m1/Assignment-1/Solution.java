2dae9d083eaebf5ec153ebd9bb39074d5f401b65
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-27 20:41:23 +0530

    name and roll number

---
+++ cspp2-assignments/m1/Assignment-1/Solution.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String name = scan.nextLine();
+		int rollnumber = scan.nextInt();
+		System.out.println(name+ "," + rollnumber);
+	}
+}
