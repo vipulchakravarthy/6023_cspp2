3b30ce1fd4be9b2d7dea65ef2435ff7969ff6e61
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-28 16:00:44 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
 /**the program is to give the roots of a quadratic equation.
 */
-public class Solution{
-	private Solution() {
-		/** the constructor for performing this program.
-		*/
-	}
-	public static void rootsOfQuadraticEquation(final int a,final int b,final int c){
-		/**the function takes the co-efficient a,b and c.
-		* prints the roots.
-		*/
-		int four = 4;
-		double dis = Math.sqrt((b * b) - (four * a * c));
-		double root1 = (-b + dis) / (2 * a);
-		double root2 = (-b - dis) / (2 * a);
-		System.out.println(root1+" "+root2);
-	}
-	public static void main(String[] args){
-		/** the main program will read the input from the user.
-		*  calls the function to get the roots of equation.
-		*/
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c); 
-	}
+public final class Solution {
+    /** the class solution is defined to find roots of.
+    *equation.
+    */
+    private Solution() {
+        /** the constructor for performing this program.
+        */
+    }
+    public static void rootsOfQuadraticEquation(final int a,final int b,final int c) {
+        /**the function takes the co-efficient a,b and c.
+        * prints the roots.
+        */
+        int four = 4;
+        double dis = Math.sqrt((b * b) - (four * a * c));
+        double root1 = (-b + dis) / (2 * a);
+        double root2 = (-b - dis) / (2 * a);
+        System.out.println(root1+" "+root2);
+    }
+    public static void main(final String[] args) {
+        /** the main program will read the input from the user.
+        *  calls the function to get the roots of equation.
+        */
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c); 
+    }
 }
 
