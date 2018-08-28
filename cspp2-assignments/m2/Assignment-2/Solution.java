ffd26f14b8c56249e55e51b330b81f182c4533e1
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-28 15:55:15 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
-import java.lang.*;
-public class Solution 
-{
-	public static void rootsOfQuadraticEquation(int a, int b, int c)
-	{
-		double x = (-b + (Math.sqrt((b*b) - (4*a*c)))) / (2*a);
-		double y = (-b - (Math.sqrt((b*b) - (4*a*c)))) / (2*a);
-		System.out.println(x+" "+y);
+/**the program is to give the roots of a quadratic equation.
+*/
+public class Solution{
+	private Solution() {
+		/** the constructor for performing this program.
+		*/
 	}
-	public static void main(String[] args)
-	{
+	public static void rootsOfQuadraticEquation(final int a,final int b,final int c){
+		/**the function takes the co-efficient a,b and c.
+		* prints the roots.
+		*/
+		int four = 4;
+		double dis = Math.sqrt(b * b);
+		double root1 = (-b + (dis - (four * a * c))) / (2 * a);
+		double root2 = (-b - (dis - (four * a * c))) / (2 * a);
+		System.out.println(root1+" "+root2);
+	}
+	public static void main(String[] args){
+		/** the main program will read the input from the user.
+		*  calls the function to get the roots of equation.
+		*/
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
 		rootsOfQuadraticEquation(a, b, c);
 	}
-
 }
+
