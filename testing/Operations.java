fab5f946c7184f8fd89b94ef5bde4617f45d5263
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-28 09:07:38 +0530

    testing

---
+++ testing/Operations.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Operations
+{
+	public static void main(String[] args)
+	{
+	 Scanner scan = new Scanner(System.in);
+	 int n = scan.nextInt();
+	 int i = 0;
+	 while(i < n)
+	 {
+	 	String line = scan.nextLine();
+	 	System.out.println(line);
+	 	i++;
+	  }
+	}
+}
