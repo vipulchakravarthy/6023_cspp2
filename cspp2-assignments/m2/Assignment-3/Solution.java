5f71a9fde97517b19d4b74328da36f2bdd52b9ad
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-28 18:48:18 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -26,8 +26,7 @@ public final class Solution {
     */
     public static long power(final long base, final long exponent) {
         if (exponent == 0) {
-         return 1;
-         }
+         return 1;}
         else {
          return base * power(base, exponent - 1);
         }
