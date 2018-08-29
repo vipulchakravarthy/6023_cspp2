8948b5377835b5d3cc7f795d29360b486b6d2201
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-29 14:00:57 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 *the program is to fin the gcd of two numbers.
 */
 public final class Solution {
-	/** this is a constructor.
-	*/
-	private Solution() {
-	}
-	/** the main program is find the gcd of given two numbers.
-	 * @param args  The arguments
-	 */
+    /** this is a constructor.
+    */
+    private Solution() {
+    }
+    /** the main program is find the gcd of given two numbers.
+     * @param args  The arguments
+     */
     public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);    
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         System.out.println(gcd(n1, n2));
     }
     /**
-	*Need to write the gcd function and print the output.
+    *Need to write the gcd function and print the output.
     *@param n1 int
     *@param n2 int
      *@return int
-	*/
-	public static int gcd(final int n1, final int n2) {
-		int result = 1;
-		for (int i = 1; i <= n1 && i <= n2; ++i) {
-			if (n1 % i == 0 && n2 % i == 0) {
-				result = i;
-			}
-		}
-		return result;
-	}
+    */
+    public static int gcd(final int n1, final int n2) {
+        int result = 1;
+        for (int i = 1; i <= n1 && i <= n2; ++i) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                result = i;
+            }
+        }
+        return result;
+    }
 }
