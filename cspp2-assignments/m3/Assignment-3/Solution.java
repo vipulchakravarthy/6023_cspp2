1a27ce6400a9c007d624d2650ae78b5605f3b8ce
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-29 13:53:45 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -1,17 +1,18 @@
-import java.util.*;
-/*
-	Do not modify this main function.
-	*/
+import java.util.Scanner;
+/**
+*Do not modify this main function.
+*the program is to fin the gcd of two numbers.
+*/
 public final class Solution {
+	/** this is a constructor.
+	*/
 	private Solution() {
-		/** this is a constructor.
-		*/
 	}
-	/** the main program is find the gcd of given two numbers
+	/** the main program is find the gcd of given two numbers.
 	 * @param args  The arguments
 	 */
     public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);    
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         System.out.println(gcd(n1, n2));
@@ -20,6 +21,7 @@ public final class Solution {
 	*Need to write the gcd function and print the output.
     *@param n1 int
     *@param n2 int
+     *@return int
 	*/
 	public static int gcd(final int n1, final int n2) {
 		int result = 1;
