46ebf94018702910f6636be8231e318ae37af230
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-30 15:27:36 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,14 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
+/** the program is to perform addition of two matrices.
+*/
 public class Solution {
 	/* Fill the main function to print resultant of addition of matrices*/
+	private Solution() {
+		/** this is the constructor.
+		*/
+	}
+	/** the main program is to perform the addition of two matrices.
+	*@param args string
+	*/
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int[][] matrix1 = readmatrix(scan);
 		int[][] matrix2 = readmatrix(scan);
 		int[][] result = new int[matrix1.length][matrix1[0].length];
 		if ((matrix1.length == matrix2.length) && (matrix1[0].length == matrix2[0].length)) {
-			result = add(matrix1,matrix2);
-			for (int i = 0; i < result.length ; i ++) {
+			result = add(matrix1, matrix2);
+			for (int i = 0; i < result.length ; i++) {
 				for (int j = 0; j < result[0].length-1; j++) {
 					System.out.print(result[i][j] + " ");
 				}
@@ -18,7 +27,10 @@ public class Solution {
 		else 
 			System.out.println("not possible");
 	}
-	
+	/** the function is to read the matrices.
+	*@param scan object of scanner
+	*@return array 
+	*/
 	public static int[][] readmatrix(Scanner scan) {
 		int m,n;
 		m = scan.nextInt();
@@ -31,9 +43,14 @@ public class Solution {
 	   }
 		return array;
 	}
+	/** the function is two add two matrices.
+	*@param matrix1 array
+	*@param matrix2 array
+	*@return result array
+	*/
 	public static int[][] add(int[][] matrix1, int[][] matrix2) {
 		int[][] result = new int[matrix1.length][matrix1[0].length];
-		for (int i = 0; i < matrix1.length ; i ++) {
+		for (int i = 0; i < matrix1.length ; i++) {
 			for (int j = 0; j < matrix1[0].length; j++) {
 				result[i][j] = matrix1[i][j] + matrix2[i][j];
 			}		
