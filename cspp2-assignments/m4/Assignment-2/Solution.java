1d5e5cb1637e20a5c4e3703d3c23ebabee1835eb
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-30 15:39:45 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 /** the program is to perform addition of two matrices.
 */
-public class Solution {
-	/* Fill the main function to print resultant of addition of matrices*/
+public final class Solution {
+	/**
+	*Fill the main function to print resultant of addition of matrices.
+	*/
 	private Solution() {
 		/** this is the constructor.
 		*/
@@ -10,18 +12,18 @@ public class Solution {
 	/** the main program is to perform the addition of two matrices.
 	*@param args string
 	*/
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int[][] matrix1 = readmatrix(scan);
 		int[][] matrix2 = readmatrix(scan);
 		int[][] result = new int[matrix1.length][matrix1[0].length];
 		if ((matrix1.length == matrix2.length) && (matrix1[0].length == matrix2[0].length)) {
 			result = add(matrix1, matrix2);
-			for (int i = 0; i < result.length ; i++) {
-				for (int j = 0; j < result[0].length-1; j++) {
-					System.out.print(result[i][j] + " ");
+			for (int i = 0; i < result.length; i++) {
+				for (int j = 0; j < result[0].length - 1; j++) {
+					System.out.print(result[i][j]+" ");
 				}
-				System.out.println(result[i][result[0].length-1]);
+				System.out.println(result[i][result[0].length - 1]);
 			 }
 		}
 		else 
@@ -31,7 +33,7 @@ public class Solution {
 	*@param scan object of scanner
 	*@return array 
 	*/
-	public static int[][] readmatrix(Scanner scan) {
+	public static int[][] readmatrix(final Scanner scan) {
 		int m,n;
 		m = scan.nextInt();
 		n = scan.nextInt();
@@ -48,9 +50,9 @@ public class Solution {
 	*@param matrix2 array
 	*@return result array
 	*/
-	public static int[][] add(int[][] matrix1, int[][] matrix2) {
+	public static int[][] add(final int[][] matrix1, final int[][] matrix2) {
 		int[][] result = new int[matrix1.length][matrix1[0].length];
-		for (int i = 0; i < matrix1.length ; i++) {
+		for (int i = 0; i < matrix1.length; i++) {
 			for (int j = 0; j < matrix1[0].length; j++) {
 				result[i][j] = matrix1[i][j] + matrix2[i][j];
 			}		
