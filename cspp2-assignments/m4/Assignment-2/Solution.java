923feda56a066f32eb728fd85830e44bff412323
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-30 15:06:55 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -2,8 +2,9 @@ import java.util.*;
 public class Solution {
 	/* Fill the main function to print resultant of addition of matrices*/
 	public static void main(String[] args) {
-		int[][] matrix1 = readmatrix();
-		int[][] matrix2 = readmatrix();
+		Scanner scan = new Scanner(System.in);
+		int[][] matrix1 = readmatrix(scan);
+		int[][] matrix2 = readmatrix(scan);
 		int[][] result = new int[matrix1.length][matrix1[0].length];
 		if ((matrix1.length == matrix2.length) && (matrix1[0].length == matrix2[0].length)) {
 			result = add(matrix1,matrix2);
@@ -17,12 +18,11 @@ public class Solution {
 		else 
 			System.out.println("Not Possible");
 	}
-	static Scanner scan = new Scanner(System.in);
-	public static int[][] readmatrix() {
+	
+	public static int[][] readmatrix(Scanner scan) {
 		int m,n;
 		m = scan.nextInt();
 		n = scan.nextInt();
-		System.out.println(m+n);
 		int[][] array = new int[m][n];
 		for (int i = 0; i < m; i++) {
 			for(int j = 0; j < n; j++) {
