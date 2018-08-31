a74a72aaaeb1bbf7797f728e0b73375360aa1180
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-08-31 11:16:02 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -1,29 +1,31 @@
 import java.util.Scanner;
+/** the program is to perform the reverse operation of a string.
+*/
 public final class Solution {
-	private Solution() {
-		/** this a constructor.
-		*/
-	}
-	/**
-	*the main program is to find the reverse of a string.
-	*@param args String
-	*/
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);	
-	}
-	/** the function is to reverse  the string.
-	*@param s String
-	*@return reverse
-	*/
-	public static String reverseString(final String s) {
-		String reverse = "";
-		for (int i = (s.length() - 1); i >= 0; i--) {
-			reverse = reverse + s.charAt(i);
-		}
-		return reverse;
-	}
+        /**this a constructor for this program. 
+        */
+    private Solution() {
+    }
+    /**
+    *the main program is to find the reverse of a string.
+    *@param args String
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    /** the function is to reverse  the string.
+    *@param s String
+    *@return reverse
+    */
+    public static String reverseString(final String s) {
+        String reverse = "";
+        for (int i = (s.length() - 1); i >= 0; i--) {
+            reverse = reverse + s.charAt(i);
+        }
+        return reverse;
+    }
 
 }
