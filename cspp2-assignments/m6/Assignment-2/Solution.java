5a412ed889cbdcab2bbd0f1eb6848a30067a01fa
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-01 10:38:28 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-2/Solution.java
+++ cspp2-assignments/m6/Assignment-2/Solution.java
@@ -23,26 +23,36 @@ final class Solution {
      * @return     Matrix of the rounded elements
      */
     static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
-    final int[] multiples = {50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550};
+        final int fifty = 50;
+        final int hundred = 100;
+        final int one_fifty =150;
+        final int two_hundred = 200;
+        final int two_fifty = 250;
+        final int three_hundred = 300;
+        final int three_fifty = 350;
+        final int four_hundred = 400;
+        final int four_fifty = 450;
+        final int five_hundred = 500;
+        final int five_fifty = 550;
 	for (int i = 0; i < rows; i++) {
         for (int j = 0; j< columns; j ++) {
-            if (a[i][j] <  multiples[0]) {
+            if (a[i][j] <  fifty) {
                 a[i][j] = 0;
             }
-            if (a[i][j] >multiples[0] && a[i][j] <= multiples[2]) {
-                a[i][j] = multiples[1];
+            if (a[i][j] >fifty && a[i][j] <= one_fifty) {
+                a[i][j] = hundred;
             }
-            if (a[i][j] > multiples[2] && a[i][j] <= multiples[4]) {
-                a[i][j] = multiples[3];
+            if (a[i][j] > one_fifty && a[i][j] <= two_fifty) {
+                a[i][j] = two_hundred;
             }
-            if (a[i][j] > multiples[4] && a[i][j] <= multiples[6]) {
-                a[i][j] = multiples[5];
+            if (a[i][j] > two_fifty && a[i][j] <= three_fifty) {
+                a[i][j] = three_hundred;
             }
-            if (a[i][j] > multiples[6] && a[i][j] <= multiples[8]) {
-                a[i][j] = multiples[7];
+            if (a[i][j] > three_fifty && a[i][j] <= four_fifty) {
+                a[i][j] = four_hundred;
             }
-            if (a[i][j] > multiples[8] && a[i][j] <= multiples[10]) {
-                a[i][j] = multiples[9];
+            if (a[i][j] > four_fifty && a[i][j] <= five_fifty) {
+                a[i][j] = five_hundred;
             }
         }
     }
