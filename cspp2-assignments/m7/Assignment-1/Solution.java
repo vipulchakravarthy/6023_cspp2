16b55aba464df728b47ec40898ae9c339a24547d
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-03 18:38:54 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -4,41 +4,42 @@ import java.util.Scanner;
  * of length six or else false.
  */
 class InputValidator {
-	static String input;
-	static String text;
-	/**
-	 * the constructor is to initialize the input given.
-	 * @param      input  given text input
-	 */
-	public InputValidator(String input) {
-		this.input = input;
-	}
-	/**
-	 *the validateData method is to either it is valid or not. if it is valid return
-	 *true or else false.
-	 * @return  boolean either true or false.
-	 */
-	public boolean validateData() {
-		int length = this.input.length();
-		final int six = 6;
-		if (length >= six) {
-			return true;
-		}
-		return false;
-	}
+    private String input;
+    private String text;
+    /**
+     * the constructor is to initialize the input given.
+     * @param      input  given text input
+     */
+    public InputValidator(final String input) {
+        this.input = input;
+    }
+    /**
+     *the validateData method is to either it is valid or not. if it is valid return
+     *true or else false.
+     * @return  boolean either true or false.
+     */
+    public boolean validateData() {
+        int length = this.input.length();
+        final int six = 6;
+        if (length >= six) {
+            return true;
+        }
+        return false;
+    }
 }
 /**
  * this solution class is to write the main program.
  */
 public class Solution {
-	/**
-	 * In this main program it reads the input from the user and creates
-	 * an object and call the method called ValidateData.
-	 */
-	public static void main(String args[]) {
-    	Scanner scan = new Scanner(System.in);
-    	String input = scan.next();
-    	InputValidator text = new InputValidator(input);    	
-    	System.out.println(text.validateData());
+    /**
+     * In this main program it reads the input from the user and creates
+     * an object and call the method called ValidateData.
+     * @param args String
+     */
+    public static void main(final String args[]) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        InputValidator text = new InputValidator(input);        
+        System.out.println(text.validateData());
     }
 }
