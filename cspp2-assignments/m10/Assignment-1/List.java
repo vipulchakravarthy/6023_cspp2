bb040be63ec05d6f51c447be89be2d1a970e1dfe
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-06 19:22:01 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/List.java
@@ -225,6 +225,9 @@ public final class List {
             str = str + list[i] + ",";
         }
         str = str + list[i] + "]";
+        if (list[0] == 0) {
+            str += "\nInvalid Position Exception";
+        }
         return str;
     }
     /**
