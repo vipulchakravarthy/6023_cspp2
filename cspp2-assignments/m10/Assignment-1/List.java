6a2a2aa83166e06e231edacd3ae7e722b9c47d2f
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-06 09:38:19 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/List.java
@@ -232,7 +232,7 @@ public class List {
      */
     public String toString() {
         if(size == 0)
-            return "";
+            return "[]";
         String str = "[";
         int i = 0;
         for(i = 0; i < size - 1; i++) {
@@ -281,7 +281,7 @@ public class List {
         if (countOf > 0) {
             return countOf;
         }
-        return -1;
+        return countOf;
     }
     /**Inserts the specified element at the specified index 
 	*by moving all the elements to the right.
