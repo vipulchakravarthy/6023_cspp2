eac53881b7ec3d6e0a3f46114f5e5d2d53187b6b
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-20 19:11:30 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -79,9 +79,10 @@ public class Solution {
 				fileMatrix[i][j] = obj.similarity(obj.toText(fileList[i]), obj.toText(fileList[j]));
 			}
 		}
-		for (int i = 0; i < length; i++) {
+		for (int i = 0; i < length - 1; i++) {
 			System.out.print("\t" +fileList[i].getName() + "\t" );
 		}
+		System.out.print(fileList[length - 1].getName());
 		for (int i = 0; i < length; i++) {
 			System.out.print(fileList[i].getName() + "\t");
 			for (int j = 0; j < length; j++) {
