6101b77dbe85c60b545a9bbc0a61a52637c6379b
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-21 13:50:18 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -22,7 +22,7 @@ class Data {
 	}
 	public Map remove(String text) {
 		text = text.toLowerCase();
-		text = text.replaceAll("[^a-zA-Z ]", "");
+		text = text.replaceAll("[0-9_]", "");
 		String[] words = text.split(" ");
 		Map<String, Integer> map = new HashMap<>();
 		for (String element : words) {
