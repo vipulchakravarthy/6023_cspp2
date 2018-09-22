7a8926d7f9533a371520a5b1c72c96699c425a73
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-22 12:27:59 +0530

    "check_style_score -> 12 of 14 passed. style score: 0/1 "

--- cspp2-assignments/m25/Assignment-1/TodoistMain.java
+++ cspp2-assignments/m25/Assignment-1/TodoistMain.java
@@ -16,9 +16,9 @@ class Todoist {
 		for (int i = 0; i < taskObj.size(); i++) {
 			if (name.equals(taskObj.get(i).getAssignedTo())) {
 				if ("todo".equals(taskObj.get(i).getStatus())) {
-					if (("Important".equals(taskObj.get(i).getImportant())) && "Not Urgent".equals(taskObj.get(i).getUrgent())) {
+					if (("Important".equals(taskObj.get(i).getImportant())) && "Urgent".equals(taskObj.get(i).getUrgent())) {
 						return taskObj.get(i);
-					} else if (("Important".equals(taskObj.get(i).getImportant())) && "Urgent".equals(taskObj.get(i).getUrgent())) {
+					} else if (("Important".equals(taskObj.get(i).getImportant())) && "Not Urgent".equals(taskObj.get(i).getUrgent())) {
 						return taskObj.get(i);
 					}
 				}
